327e082ebca0838bb7ad842b839e869c82753dcc
triskelion16 <triskelion.work@gmail.com>
2020-04-15 23:28:38 +0200

    fixed errors

---
+++ src/test/java/pl/triskelion/mytwitterrestapi/MyTwitterRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.triskelion.mytwitterrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTwitterRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
